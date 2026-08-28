e3c2e7ded1d0fde58f4368539c13e36aa235aa3a
Matheus Duarte <matheus.sduarte.ms@gmail.com>
2026-08-28 00:28:52 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,10 +38,18 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-			} catch (ChessException e) {
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+			} 
+			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			} catch (InputMismatchException e) {
+			} 
+			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
